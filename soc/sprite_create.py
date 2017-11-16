5c7ef14e0b24fbe8d2d3e8184218e18957869be5
FFtust <1143943623@qq.com>
2017-11-16 11:42:57 +0800

    add sprite del

--- soc/sprite_create.py
+++ soc/sprite_create.py
@@ -16,42 +16,11 @@ class sprite_create():
         p_y_u = 0
         p_y_d = 0
         self.sprite_info = []
-        print("face string is:", sp_info)
+        #print("face string is:", sp_info)
         for i in range(FACE_CROSS):
             dat = int(sp_info[i * 2 :  (i + 1) * 2], 16)
-            print(dat)
-            self.sprite_info.append(dat)
-            if count == 0 and dat != 0:
-                p_x_l = i
-                count += 1
-            elif count == 1 and dat == 0:
-                p_x_r = i - 1
-                count += 1
-        if count == 1:
-            p_x_r = 15 
- 
-        count = 0
-        ret = 0
-        # check the lfet_up point
-        for i in range(FACE_LINE):
-            for j in range(FACE_CROSS):
-                ret |= self.sprite_info[j] & (1 << i)
-            if count == 0 and ret != 0:
-                p_y_u = i
-                count += 1
-            elif count == 1 and ret == 0:
-                p_y_d = i - 1
-                count += 1
-                break
-            if count > 2:
-                break
-            ret = 0    
 
-        if count == 1:
-            p_y_d = 7 
-        self.lu_coord = [p_x_l, p_y_u] 
-        self.rd_coord = [p_x_r, p_y_d]  
-        print("left_up:", self.lu_coord, "right_down:", self.rd_coord)
+            self.sprite_info.append(dat)
         self.rotate_angle = 0
         self.position = [0, 0]
     def add_point(self):
@@ -63,29 +32,9 @@ class sprite_create():
 # srpite control
     def left(self, num = 1):
         self.position = [self.position[0] - num, self.position[1]]
-        '''
-        p_c = self.lu_coord[0] - num
-        if p_c < 0:
-            P_ = 0
-        self.lu_coord = [p_c, self.lu_coord[1]]
-        p_c = self.rd_coord[0] - num
-        if p_c < 0:
-            p_c = 0        
-        self.rd_coord = [p_c, self.rd_coord[1]]
-        '''
 
     def right(self, num = 1):
         self.position = [self.position[0] + num, self.position[1]]
-        '''
-        p_c = self.lu_coord[0] + num
-        if p_c > FACE_CROSS:
-            P_ = FACE_CROSS
-        self.lu_coord = [p_c, self.lu_coord[1]]
-        p_c = self.rd_coord[0] + num
-        if p_c < 0:
-            p_c = FACE_CROSS        
-        self.rd_coord = [p_c, self.rd_coord[1]]
-        '''
     def up(self, num = 1):
         self.position = [self.position[0], self.position[1] - num]
 
@@ -117,11 +66,10 @@ class game_base():
 
         self.sprite_list = []
 
-    def del_all_sprite(self, sp):
-        pass
-
-    def del_sprite(self):
-        pass
+    def del_sprite(self, sp):
+        for i in range(len(self.sprite_list)):
+            if self.sprite_list[i] == sp:
+                del self.sprite_list[i]
 
     def sprite_list_clean(self):
         self.sprite_list = []
@@ -163,7 +111,7 @@ while True:
     elif codey.is_button("B"):
         a.right()
     elif codey.is_button("C"):
-        a.down()     
+        game.del_sprite(b)  
     if codey.dail() > 50:
         a.up()   
     game.screen_refresh()
